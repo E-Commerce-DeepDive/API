11c700eb350194b0104c92e018eb0f31e0413349
Ahmed <ahmedhamouda18hhh@gmail.com>
2025-09-02 23:43:45 +0300

    Add cors && Complete Last feat for order

---
+++ Ecommerce.Entities/Shared/OrderFilters.cs
@@ -0,0 +1,9 @@
+﻿using Ecommerce.Utilities.Enums;
+
+namespace Ecommerce.Entities.DTO.Shared;
+public class OrderFilters<TSortColumn> : RequestFilters<TSortColumn>
+    where TSortColumn : struct, Enum
+
+{
+    public OrderStatus? Status { get; set; }
+}
